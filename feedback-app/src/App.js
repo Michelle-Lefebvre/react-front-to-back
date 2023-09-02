8c1b83daea5d4e728e4bd8fac1b46a35a692c667
Michelle Lefebvre <mmldesigns007@gmail.com>
2023-09-01 20:48:18 -0400

    array.map

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment 1" },
+    { id: 2, text: "Comment 2" },
+    { id: 3, text: "Comment 3" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
